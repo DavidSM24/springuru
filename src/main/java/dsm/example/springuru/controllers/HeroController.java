b12464e00b6a20f334ea29acdd68d5e78ad6b62d
dsmorales <dsmorales@indra.es>
2022-03-29 13:56:36 +0200

    update heroController With All Methods

--- src/main/java/dsm/example/springuru/controllers/HeroController.java
+++ src/main/java/dsm/example/springuru/controllers/HeroController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,11 +24,28 @@ public class HeroController {
         this.heroService = heroService;
     }
 
-    @RequestMapping("/heroes")
-    public ResponseEntity<List<Hero>> getAuthors(Model model){
+    @GetMapping("/heroes")
+    public ResponseEntity<List<Hero>> getHeroes(){
 
         List<Hero> all=heroService.getAll();
         return new ResponseEntity<List<Hero>>(all,new HttpHeaders(), HttpStatus.OK);
 
     }
+
+    @GetMapping("/heroes/id/{id}")
+    public ResponseEntity getById(@PathVariable("id") Long id){
+
+        Hero result=heroService.getById(id);
+        if(result!=null) return new ResponseEntity<Hero>(result,new HttpHeaders(), HttpStatus.OK);
+        else return new ResponseEntity("No se ha encontrado un héroe con ese id.",new HttpHeaders(), HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/heroes/name/{name}")
+    public ResponseEntity<List<Hero>> getByName(@PathVariable("name") String name){
+
+        List<Hero> all=heroService.getByName(name);
+        return new ResponseEntity<List<Hero>>(all,new HttpHeaders(), HttpStatus.OK);
+
+    }
+
 }
