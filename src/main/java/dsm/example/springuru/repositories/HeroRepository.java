4690ee4d5e5d2797a667d5f5f5e040a269718c0d
dsmorales <dsmorales@indra.es>
2022-03-29 12:44:36 +0200

    update heroRepository

--- src/main/java/dsm/example/springuru/repositories/HeroRepository.java
+++ src/main/java/dsm/example/springuru/repositories/HeroRepository.java
@@ -1,8 +1,9 @@
 package dsm.example.springuru.repositories;
 
 import dsm.example.springuru.domain.Hero;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface HeroRepository extends CrudRepository<Hero,Long> {
+public interface HeroRepository extends JpaRepository<Hero,Long> {
 
 }
