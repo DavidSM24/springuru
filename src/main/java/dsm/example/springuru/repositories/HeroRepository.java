d9084de35215945887fc5d75dfd1bc7a563fc730
dsmorales <dsmorales@indra.es>
2022-03-29 13:57:16 +0200

    update heroRepository With All Methods

--- src/main/java/dsm/example/springuru/repositories/HeroRepository.java
+++ src/main/java/dsm/example/springuru/repositories/HeroRepository.java
@@ -2,8 +2,13 @@ package dsm.example.springuru.repositories;
 
 import dsm.example.springuru.domain.Hero;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface HeroRepository extends JpaRepository<Hero,Long> {
 
+    @Query(value = "SELECT * FROM HERO WHERE LOWER(NAME) LIKE %:name%", nativeQuery = true)
+    public List<Hero> getByName(@Param("name") String name);
 }
