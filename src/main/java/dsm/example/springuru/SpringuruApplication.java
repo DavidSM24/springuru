11c8dd24695c271bb64bb0e3400817fd16cdf62b
dsmorales <dsmorales@indra.es>
2022-03-30 08:38:24 +0200

    Cors policy added

--- src/main/java/dsm/example/springuru/SpringuruApplication.java
+++ src/main/java/dsm/example/springuru/SpringuruApplication.java
@@ -2,7 +2,9 @@ package dsm.example.springuru;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 @SpringBootApplication
 public class SpringuruApplication {
 
